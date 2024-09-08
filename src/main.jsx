0cf5a48e64a9da3be631c3fda7e76a8c75866f76
Joshua <infernezor@gmail.com>
2024-09-07 21:20:10 -0400

    JB: Init upload.

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import landing from "./landing";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: landing,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

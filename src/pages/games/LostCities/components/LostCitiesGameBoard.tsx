829f4cbab4ffcced860fc8cd102c341379b73d9e
Joshua <infernezor@gmail.com>
2024-09-29 22:28:46 -0400

    jb: update to board.

--- src/pages/games/LostCities/components/LostCitiesGameBoard.tsx
+++ src/pages/games/LostCities/components/LostCitiesGameBoard.tsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import { useLostCitiesStore } from '../store/lostcitiesStore'
+import { Card } from '../LostCitiesTypes';
 const changeActivePlayer = useLostCitiesStore.getState().changeActivePlayer;
 
 export default function LostCitiesGameBoard() {
     let ActivePlayer = useLostCitiesStore(state => state.ActivePlayer)
-    console.log(ActivePlayer)
+    let CurrentRound = useLostCitiesStore(state => state.ActiveRound)
     if (ActivePlayer === undefined) {
-        changeActivePlayer(useLostCitiesStore.getState().player1) 
+        changeActivePlayer(useLostCitiesStore.getState().player1)
     }
+    console.log(ActivePlayer.RoundState[CurrentRound - 1].PlayerBoard)
+    const playerBoard = ActivePlayer.RoundState[CurrentRound - 1].PlayerBoard; // this is an object with the colors to be rendered.
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            {Object.keys(playerBoard).map((CardColor, index) => {
+                console.log(playerBoard[CardColor])
+                const colorS: Card[] = playerBoard[CardColor]
+                return (
+                    <div key={index}>
+                        <RenderColor color={colorS} />
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+function RenderColor(color: any) {
 
     return (
         <div>
-            {/* {ActivePlayer.playerName} */}
-            <h1>Lost Cities Game Board</h1>
+            {color.color.map((card: Card, index: number) => {
+                // console.log(color)
+                return (
+                    <ColorButton key={index} color={card} />
+                )
+            })}
         </div>
+    )
+}
+
+function ColorButton({ color }: { color: any }) {
+    // console.log(color)
+    return (
+        <div style={{ backgroundColor: color.color }}>{color.value}</div>
     )
 }

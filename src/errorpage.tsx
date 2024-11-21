97e96f7640a324fc99138f2908986462b5ccd4db
Joshua <infernezor@gmail.com>
2024-11-20 21:39:42 -0500

    JB: meh.

--- src/errorpage.tsx
+++ src/errorpage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ErrorPage() {
     return (
         <div>
-            <h1>404  - Page Not Found</h1>
+            <h1>404  - Page Not Found - or is it?</h1>
         </div>
     );
 }

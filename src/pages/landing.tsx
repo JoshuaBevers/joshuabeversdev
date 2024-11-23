7cc0b6db18a3afe7a8db4096559c4f85c723f20b
Joshua <infernezor@gmail.com>
2024-11-22 20:58:39 -0500

    JB:

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -4,7 +4,7 @@ function Landing() {
     return (
         <div>
             <h1 style={{marginTop: 100}}>Board Game Arena</h1>
-            <p>Base Landing page</p>
+            <p>Base Landing page.net</p>
         </div>
     );
 }
